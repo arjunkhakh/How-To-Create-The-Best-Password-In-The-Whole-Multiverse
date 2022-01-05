5a5047805ebc60c199a75e35e1c2b53d1016c4e5
Arjun Singh <arjunkhakh@gmail.com>
2022-01-05 16:23:51 +0000

    More changes to my Javascript

--- assets/javascript.js
+++ assets/javascript.js
@@ -6,23 +6,55 @@ var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var symbols = ["!","$","£","%","^","&","*","(",")","+","_","=","-","]","[","#","~","@",";",":","<",">","/","?",",","."]
 var number = ["1","2","3","4","5","6","7","8","9","0"]
 
+var passwordLength = 12;
+var generate = "";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
- 
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function box(){
-var LowercaseQuestion = confirm("Would You Like Lowercase Letters?");
-var UppercaseQuestion = confirm("Would You like Uppercase Letter?");
-var SymbolQuestion = confirm("Would You like Symbols?");
-var numberQuestion = confirm("Would You like Numeric Characters?");
+function LowerCase() {
+  confirm("Would You Like Lowercase Letters?")  
 }
+
+function UpperCase() {
+  confirm("Would You Like Uppercase Letters?")
+}
+
+function Symbols() {
+  confirm("Would You like Symbols?")
+}
+
+function Numbers() {
+  confirm("Would You like Numeric Characters?")
+}
+
+function generatePassword() {
+  LowerCase()
+  UpperCase()
+  Symbols()
+  Numbers()
+}
+
+
+  // for (var i = 0; i <= passwordLength; i++) {
+  //  var randomNumber = Math.floor(Math.random() * symbols.length);
+  //  generate += symbols.substring(randomNumber, randomNumber +1);
+
+  // }
+
+
+
+// document.getElementById("generate").value = generate;
+
+
+
+
 
